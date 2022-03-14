caaceacbb94b891549a0b760361b3249c3613579
giancarlol <giancarlobrusca@gmail.com>
2022-03-14 02:26:40 -0300

    delete event & igpost (for now)

--- schemas/schema.js
+++ schemas/schema.js
@@ -1,21 +1,17 @@
 // First, we must import the schema creator
-import createSchema from 'part:@sanity/base/schema-creator'
+import createSchema from "part:@sanity/base/schema-creator";
 
 // Then import schema types from any plugins that might expose them
-import schemaTypes from 'all:part:@sanity/base/schema-type'
-import person from './person'
-import igpost from './igpost'
-import gallery from './gallery'
+import schemaTypes from "all:part:@sanity/base/schema-type";
+import person from "./person";
+import igpost from "./igpost";
+import gallery from "./gallery";
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
   // We name our schema
-  name: 'preservate',
+  name: "preservate",
   // Then proceed to concatenate our document type
   // to the ones provided by any plugins that are installed
-  types: schemaTypes.concat([
-    person,
-    igpost,
-    gallery
-  ]),
-})
+  types: schemaTypes.concat([person]),
+});
