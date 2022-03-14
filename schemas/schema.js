6869562f6315db271bdab9e77b49b0482ebf9df8
giancarlol <giancarlobrusca@gmail.com>
2022-03-14 02:32:37 -0300

    delete unused import from schemas

--- schemas/schema.js
+++ schemas/schema.js
@@ -4,8 +4,6 @@ import createSchema from "part:@sanity/base/schema-creator";
 // Then import schema types from any plugins that might expose them
 import schemaTypes from "all:part:@sanity/base/schema-type";
 import person from "./person";
-import igpost from "./igpost";
-import gallery from "./gallery";
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
